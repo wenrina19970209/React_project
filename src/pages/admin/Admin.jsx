9608311831f02ce6ba00e5299f861e1d99636fbf
haha <haha@121.com>
2019-06-14 18:28:34 +0800

    router

---
+++ src/pages/admin/Admin.jsx
@@ -0,0 +1,13 @@
+/* 
+后台管理界面
+*/
+
+import React from 'react'
+
+export default class Admin extends React.Component{
+    render(){
+        return (
+            <div>Admin</div>
+        )
+    }
+}
---
+++ src/pages/login/Login.jsx
@@ -0,0 +1,13 @@
+/* 
+登录界面
+
+*/
+import React from 'react'
+
+export default class Login extends React.Component{
+    render(){
+        return(
+            <div>Login</div>
+        )
+    }
+}
