26542f77a8d9b8e08c9fd2eacc7c14c1921cd47b
haha <haha@121.com>
2019-06-15 12:43:02 +0800

    收集数据

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,15 +1,50 @@
 /* 
 登录界面
-
+Fragment
 */
 import React,{Component} from 'react'
 import logo from './images/logo.png'
 import { Form, Icon, Input, Button } from 'antd';
 import './login.less'
+const Item = Form.Item
 
-export default class Login extends Component{
+ class Login extends Component{
 
+    handleSubmit = (event)=>{
+        event.preventDefault();
+
+        this.props.form.validateFields((err,values)=>{
+            if(!err){
+                console.log('成功发送Ajax请求！',values)
+            }
+        })
+        
+
+        /* let username = this.props.form.getFieldValue('username')
+        let password = this.props.form.getFieldValue('password')
+        let values= this.props.form.getFieldsValue()
+        console.log(username,password,values) */
+    }
+
+    
+// validateFields
+
+    validatorPwd = (rule, value, callback)=>{
+        if(!value){
+            callback('密码不能为空！')
+        }else if(value.length < 4){
+            callback('密码不能小于4位！')
+        }else if(value.length > 12){
+            callback('密码不能大于12位！')
+        }else if(!/^[a-zA-Z0-9_]+$/.test(value)){
+            callback('密码必须是字母、数字、下划线！')
+        }else{
+            callback()
+        }
+    }
+    
     render(){
+        let {getFieldDecorator} = this.props.form;
         return (
             <div className='login'>
                 <header className='login-header'>
@@ -19,27 +54,57 @@ export default class Login extends Component{
                 <section className='login-content'>
                     <h2>User login to lu</h2>
                     <Form onSubmit={this.handleSubmit} className="login-form">
-                        <Form.Item>
-                            <Input
-                                prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
-                                placeholder="Username"
+                        <Item>
+                            {
+                                getFieldDecorator('username',{//配置对象
+
+                                    initialValue:'',
+                                   
+                                    rules:[
+                                        {required:true,message:'请输入用户名！'},
+                                        {min:4,message:'用户名不能小于4位'},
+                                        {max:12,message:'用户名不能大于12位'},
+                                        {pattern:/^[a-zA-Z0-9_]+$/,message:'用户名必须是字母数字下划线'},
+                                    ]
+                                })(
+                                 <Input
+                                    prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                                    placeholder="Username"
                                 />
-                        </Form.Item>
-                        <Form.Item>
-                            <Input
-                                prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
-                                type="password"
-                                placeholder="Password"
-                                />
-                        </Form.Item>
-                        <Form.Item>
+                                )
+                            }
+                            
+                        </Item>
+                        <Item>
+                            {
+                                getFieldDecorator('password',{
+                                   rules:[
+                                    {validator:this.validatorPwd}
+                                   ]
+                                })(
+                                    <Input
+                                    prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                                    type="password"
+                                    placeholder="Password"
+                                    />
+                                )
+                            }
+                           
+                        </Item>
+                        <Item>
                             <Button type="primary" htmlType="submit" className="login-form-button">
                                 Log in
                             </Button>
-                        </Form.Item>
+                        </Item>
                     </Form>
                 </section>
             </div>
         )
     }
 }
+
+//返回函数包装了一个From组件生成了一个新的组件
+const UserLogin = Form.create()(Login);
+export default UserLogin 
+
+
